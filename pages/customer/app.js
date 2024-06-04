4799b7d371ec52b88e4a30cb042eeb350b8264ac
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2024-06-04 16:41:09 +0500

    customers

--- pages/customer/app.js
+++
@@ -1,6 +0,0 @@
-// let navLinks = document.querySelector('.navLinks');
-// let hamburgerMenu = document.querySelector('.hamburgerMenu');
-
-// hamburgerMenu.addEventListener('click', () => {
-//   navLinks.classList.toggle('open');
-// });
